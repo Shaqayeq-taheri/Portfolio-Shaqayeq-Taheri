25841edc2f26efd486423b0b98f0eb3fd5b9b70b
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-12-25 10:31:02 +0100

    about me page is fully responsive

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-
 function Aboutme() {
   return (
     <div className="">
         <h2 className="text-4xl text-center font-bold bg-slate-500 pt-10 ">About Me</h2>
-      <div className="flex items-center justify-center bg-slate-500 flex-wrap " style={{ height: '38rem' }} > 
+      <div className="flex items-center justify-center bg-slate-500 flex-wrap md:gap-24" style={{ height: '38rem' }} > 
          
-      <div  className="flex flex-col justify-center  bg-slate-500 md:gap-5 sm:gap-3  md:w-1/2 sm:p-5 " >
+      <div  className="flex flex-col justify-center p-10 md:p-5 bg-slate-500 gap-10 md:w-1/2 " >
         {/* w-1/2 means style={{width:'50%'}} */}
         
   
-         <p className="text-gray-100 text-xl dark:text-gray-400 bg-slate-500 max-w-xlg"> I am Shaqayeq Taheri, Junior Full-stack Web Developer, with a strong background in computer engineering, over than 2 years of experience in IT, proficient in a range of techologies in both Front-end and Back-end development. </p>
-<button  className="text-xl   w-48 bg-rose-100 hover:bg-rose-200 transition delay-100 p-4 ">My CV</button>
+         <p className="text-gray-100 text-xl text-center md:text-left dark:text-gray-400 bg-slate-500 max-w-xlg"> I am Shaqayeq Taheri, Junior Full-stack Web Developer, with a strong background in computer engineering, over than 2 years of experience in IT, proficient in a range of techologies in both Front-end and Back-end development. </p>
+<button  className="text-xl mx-auto md:mx-0  w-48 bg-rose-100 hover:bg-rose-200 transition delay-100 p-4 ">My CV</button>
       
       </div>         
-    <img src="src/assets/images/myImage.jpg" alt="" className="  rounded-full lg:order-last xs:order-first xs:ml-3 xs:mb-3" style={{width:'200px', height:'210px'}} />
+    <img src="src/assets/images/myImage.jpg" alt="" className="  rounded-full order-first md:order-last mb-2 mt-10 md:mt-0 md:mb-20" style={{width:'200px', height:'210px' }} />
+    {/* I used the md:order-last class, which will override the order-first class for screen sizes greater than or equal to 768px. */}
     </div>
     </div>
     
