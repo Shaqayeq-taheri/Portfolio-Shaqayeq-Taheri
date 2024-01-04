909c2c3dac51d4f669860c9464f1ffb4678ab35a
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2024-01-04 23:51:22 +0100

    fixing the responsivity of contact

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,10 +5,10 @@ import { HiOutlineMail } from "react-icons/hi";
 function ContactMe() {
   return (
     <div className='bg-zinc-100 p-20'>
-      <h5 className="text-4xl text-center bg-zinc-100 font-bold tracking-tight">Contact Me</h5>
-      <div className='flex gap-10 bg-zinc-100'>
-        <img src="src/assets/images/contact.svg" className='bg-zinc-100  w-1/2 h-1/2' alt="contact me" />
-        <div className='flex flex-col mx-auto justify-center gap-10 bg-zinc-100  mt-44'>
+      <h5 className="text-4xl text-center bg-zinc-100 font-bold ">Contact Me</h5>
+      <div className='flex flex-wrap gap-10 bg-zinc-100 '>
+        <img src="src/assets/images/contact.svg" className='bg-zinc-100 ' alt="contact me" />
+        <div className='flex md:flex-col mx-auto justify-center gap-10 bg-zinc-100 md:mt-44 mt-10'>
         <a href="https://www.linkedin.com/in/shaqayeq-taheri-328697254/">
           <FaLinkedin size={80} className='hover:animate-pulse' />
         </a>
